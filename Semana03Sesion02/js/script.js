cffdb36c75e42f24b59c0e6e391f231eae2cbfd3
milton cesar <milsar17@gmail.com>
2025-04-27 17:08:32 -0500

    mi primera pagina

---
+++ Semana03Sesion02/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById('btb').addEventListener('click', (e) => {
+    e.preventDefault();
+    alert("Hola desde el click del boton");
+});
+
+document.getElementById('btn2').addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('hola de nuevo');
+});
