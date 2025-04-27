4198aaf116e95ff055d9fbbde1b3d678fbe5962d
milton cesar <milsar17@gmail.com>
2025-04-27 17:32:24 -0500

    ejercicios en html

--- Semana03Sesion02/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('btb').addEventListener('click', (e) => {
-    e.preventDefault();
-    alert("Hola desde el click del boton");
-});
-
-document.getElementById('btn2').addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('hola de nuevo');
-});
